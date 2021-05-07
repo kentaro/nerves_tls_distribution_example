9d5c2485a8b64b75031536a92602672f92a08f31
Kentaro Kuribayashi <kentarok@gmail.com>
2021-05-07 18:05:19 +0900

    nerves_tls_distribution_example/

---
+++ lib/nerves_tls_distribution_example.ex
@@ -0,0 +1,18 @@
+defmodule NervesTlsDistributionExample do
+  @moduledoc """
+  Documentation for NervesTlsDistributionExample.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesTlsDistributionExample.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_tls_distribution_example_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesTlsDistributionExampleTest do
+  use ExUnit.Case
+  doctest NervesTlsDistributionExample
+
+  test "greets the world" do
+    assert NervesTlsDistributionExample.hello() == :world
+  end
+end
